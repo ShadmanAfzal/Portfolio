36c7575be8f37a98da140f73083831fce8a0d73a
Shadman <shadman.afzal.7@gmail.com>
2020-05-27 01:16:32 +0530

    improved logic add index.js"

---
+++ index.js
@@ -0,0 +1,16 @@
+var button = document.getElementById("btt");
+
+window.onscroll = function() { scrollfuncion() };
+
+function scrollfuncion() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function backtotop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
