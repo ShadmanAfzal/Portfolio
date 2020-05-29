f9dfdd53bcfd9024a8ee4bc5b7ad7a562c5329b9
Shadman Afzal Nawab <shadman.afzal.7@gmail.com>
2020-05-29 13:56:09 +0530

    improved changes

--- index.js
+++ index.js
@@ -8,19 +8,7 @@ function scrollfuncion() {
     } else {
         button.style.display = "none";
     }
-    // if (document.documentElement.scrollTop > 2240) {
-    //     console.log("reached");
-    //     var bar = document.querySelectorAll(".skill-bar-in");
-    //     for (let i = 0; i < 5; i++) {
-    //        for (let j = 0; j < 90; j++) {
-    //            console.log(i+"\n");
-    //            bar[i].style.width = j+"%";
-    //            console.log(j+"%");
-    //        }
-            
-    //     }
-    // }
-}
+    }
 
 function backtotop() {
     document.body.scrollTop = 0;
